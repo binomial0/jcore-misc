72b4d7e0197ed22186b20445434518172e365040
Elena Grygorova <elena.grygorova@uni-jena.de>
2017-08-30 14:42:19 +0200

    Update updateLicense.py

--- jcore-scripts/updateLicense.py
+++ jcore-scripts/updateLicense.py
@@ -3,12 +3,25 @@ import re
 import os
 import sys
 
-
 #function which replaces the line in the license header
-def replaceHeader(path):
+def replaceLineInHeader(path):
 	with fileinput.FileInput(path, inplace=True) as newFile:
 		for line in newFile:
 			print(line.replace("GNU Lesser General Public License (LGPL) v3.0", "BSD-2-Clause License"), end = "")
+	print("replaced line in license header in: " + path)
+
+#function which adds new license header if first line is 'package'
+def addNewLicenseHeader(path, licenseHeader):
+ with open(licenseHeader, "r") as licFile:
+	 	licRead = licFile.readlines()
+ with open(path, "r") as newFile:
+	 	newRead = newFile.readlines()
+ newRead.insert(0, licRead)
+ with open(path, "w") as newFile:
+		for item in newRead:
+	 		 newFile.writelines(item)
+ print("added new license header in: " + path)
+	
  
 #function which replaces whole license text in the LICENSE files
 def replaceLicenseText(path, newLicenseFile):
@@ -18,27 +31,35 @@ def replaceLicenseText(path, newLicenseFile):
 		for item in licRead:
 			newFile.write(item)
 
-
-#####################################################################################################################
-
-if len(sys.argv) != 3:
-	print("usage: updateLicense.py <path to directory> <path to license file>")
+#function which gets first line of a given file
+def getfirstline(path):
+	with open(path, 'r') as fi:
+		firstline = fi.readline()
+	return firstline
+#main
+if len(sys.argv) != 4:
+	print("usage: updateLicense.py <path to directory> <path to license file> <path to new license header template>")
 
 else: 
  srcDir = str(sys.argv[1]) # source directory
  newLicenseFile = str(sys.argv[2]) # path to the file with the new license content
-#walk recursively through source Directory
+ newLicenseHeader = str(sys.argv[3]) #path to the file with new license header
+ #walk recursively through source Directory
  for dirpath, dirs, files in os.walk(srcDir):	
 	 for filename in files:
-		 basicpath = os.path
 		 path = os.path.join(dirpath,filename)
 		 if filename.startswith("LICENSE") and "/target" not in path:
 			 replaceLicenseText(path, newLicenseFile)
 			 print("Replaced old Content in License File in :" + path)
-		 if "/de/julielab" in path:
+		 if "/de/julielab" in path and not "/jcore-types/" in path:
 			 if filename.endswith(".java"):
-				 print("Replaced Header in File: " + path)
-				 replaceHeader(path)
+				 newline = getfirstline(path)
+				 if newline.startswith("/*"):
+				 	 replaceLineInHeader(path)
+				 elif newline.startswith("package"):
+				 	 addNewLicenseHeader(path, newLicenseHeader)
+				 else:
+					 print("WARNING! NO APPROPRIATE HEADER!" + path)
 		 else:
 			 if filename.endswith(".java"):
-				 print("WARNING: NOT A /de/julielab DIRECTORY! " + path)
+				 print("WARNING: NOT A /de/julielab DIRECTORY!" + path)
