91b59a272e352472027d9f06c647b5255b906b7e
khituras <chew@gmx.net>
2018-02-01 17:30:17 +0100

    Adding a small project for automatic creation of component descriptors.
    Works for UIMAfit-enabled components. The project must be set as a
    dependency for the component project. Then, run the main method of the
    DescriptorCreator. This can be done via Eclipse run configurations.

---
+++ jcore-descriptor-creator/src/main/java/de/julielab/jcore/misc/DescriptorCreator.java
@@ -0,0 +1,107 @@
+package de.julielab.jcore.misc;
+
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.Writer;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.apache.uima.analysis_component.AnalysisComponent;
+import org.apache.uima.analysis_engine.AnalysisEngineDescription;
+import org.apache.uima.collection.CollectionReader;
+import org.apache.uima.collection.CollectionReaderDescription;
+import org.apache.uima.fit.factory.AnalysisEngineFactory;
+import org.apache.uima.fit.factory.CollectionReaderFactory;
+import org.apache.uima.resource.ResourceCreationSpecifier;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.xml.sax.SAXException;
+
+import de.julielab.java.utilities.FileUtilities;
+import io.github.lukehutch.fastclasspathscanner.FastClasspathScanner;
+
+public class DescriptorCreator {
+
+
+	private static final Logger log = LoggerFactory.getLogger(DescriptorCreator.class);
+
+	public static final String DEFAULT_OUTPUT_ROOT = "src" + File.separator + "main" + File.separator + "resources";
+	private static final String DESC = "desc";
+
+	public static void main(String[] args) throws Exception {
+		DescriptorCreator creator = new DescriptorCreator();
+		creator.run();
+	}
+
+	public void run() throws Exception {
+		run(DEFAULT_OUTPUT_ROOT);
+	}
+
+	public void run(String outputRoot) throws Exception {
+		List<Class<? extends CollectionReader>> readers;
+		List<Class<? extends AnalysisComponent>> aes;
+		readers = findSubclasses(CollectionReader.class);
+		aes = findSubclasses(AnalysisComponent.class);
+
+		readers = readers.stream().filter(c -> c.getPackage().getName().contains("de.julielab.jcore.reader"))
+				.collect(toList());
+		aes = aes.stream().filter(c -> c.getPackage().getName().contains("de.julielab.jcore.ae")
+				|| c.getPackage().getName().contains("de.julielab.jcore.consumer")).collect(toList());
+
+		if (readers.isEmpty() && aes.isEmpty()) {
+			log.warn("No JCoRe UIMA component classes were found.");
+		} else {
+			if (readers.size() + aes.size() > 1) {
+				log.warn(
+						"Multiple JCoRe UIMA component classes were found: {} {}. Multiple descriptors will be created with a running index. Manual curation will be required.",
+						readers, aes);
+			}
+			int num = 0;
+			for (Class<? extends CollectionReader> cls : readers) {
+				CollectionReaderDescription d = CollectionReaderFactory.createReaderDescription(cls);
+				num += writeComponentDescriptor(outputRoot, cls, d, "collection reader", num);
+			}
+			for (Class<? extends AnalysisComponent> cls : aes) {
+				AnalysisEngineDescription d = AnalysisEngineFactory.createEngineDescription(cls);
+				num += writeComponentDescriptor(outputRoot, cls, d, "analysis engine / consumer", num);
+			}
+		}
+	}
+
+	private <T> List<Class<? extends T>> findSubclasses(Class<? extends T> cls) {
+		List<Class<? extends T>> components;
+		// consumers are also analysis components
+		components = new ArrayList<>();
+		FastClasspathScanner fcs = new FastClasspathScanner();
+		fcs.matchClassesImplementing(cls, components::add).scan();
+		return components;
+	}
+
+	private int writeComponentDescriptor(String outputRoot, Class<?> cls, ResourceCreationSpecifier d,
+			String componentType, int num) throws SAXException, IOException {
+		String componentName = getComponentName();
+		String filename = componentName;
+		if (num > 0)
+			filename += num;
+		filename += ".xml";
+		List<String> pathElements = Arrays.asList(outputRoot,
+				cls.getPackage().getName().replaceAll("\\.", File.separator), DESC, filename);
+		File outputPath = new File(pathElements.stream().collect(joining(File.separator)));
+		if (!outputPath.getParentFile().exists())
+			outputPath.getParentFile().mkdirs();
+		log.info("Writing {} descriptor from class {} to {}", componentType, cls, outputPath);
+		try (Writer w = FileUtilities.getWriterToFile(outputPath)) {
+			d.toXML(w);
+			++num;
+		}
+		return num;
+	}
+
+	public static String getComponentName() {
+		return new File(".").getAbsoluteFile().getParentFile().getName();
+	}
+}
---
+++ jcore-descriptor-creator/src/test/java/de/julielab/jcore/misc/DescriptorCreatorTest.java
@@ -0,0 +1,45 @@
+package de.julielab.jcore.misc;
+
+import static java.util.stream.Collectors.joining;
+import static org.junit.Assert.assertTrue;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.Optional;
+import java.util.stream.Stream;
+
+import org.apache.commons.io.FileUtils;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class DescriptorCreatorTest {
+	
+	@BeforeClass
+	@AfterClass
+	public static void shutdown() throws IOException {
+		FileUtils.deleteDirectory(new File(Arrays.asList("src", "test", "resources", "de").stream().collect(joining(File.separator))));
+	}
+	@Test
+	public void testRun() throws Exception {
+		DescriptorCreator creator = new DescriptorCreator();
+		String outputRoot = "src" + File.separator + "test" + File.separator + "resources" + File.separator;
+		creator.run(outputRoot);	
+		File crDir = new File(outputRoot + Stream.of("de", "julielab", "jcore", "reader", "testreader", "desc").collect(joining(File.separator)));
+		File aeDir = new File(outputRoot + Stream.of("de", "julielab", "jcore", "ae", "testae", "desc").collect(joining(File.separator)));
+		File consumerDir = new File(outputRoot + Stream.of("de", "julielab", "jcore", "consumer", "testconsumer", "desc").collect(joining(File.separator)));
+		
+		assertTrue(crDir.exists());
+		assertTrue(aeDir.exists());
+		assertTrue(consumerDir.exists());
+		
+		assertTrue(containsDescriptor(crDir));
+		assertTrue(containsDescriptor(aeDir));
+		assertTrue(containsDescriptor(consumerDir));
+	}
+
+	private boolean containsDescriptor(File dir) {
+		return Stream.of(dir.list()).filter(f -> f.contains(DescriptorCreator.getComponentName())).findAny().isPresent();
+	}
+}
