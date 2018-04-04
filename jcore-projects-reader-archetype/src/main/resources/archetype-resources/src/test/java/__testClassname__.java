407a4f4a3f343048d68149d646b735b26fa686d0
khituras <chew@gmx.net>
2018-04-04 13:48:08 +0200

    Adding an archetype for creating JCoRe reader projects - i.e. for the projects repository

---
+++ jcore-projects-reader-archetype/src/main/resources/archetype-resources/src/test/java/__testClassname__.java
@@ -0,0 +1,22 @@
+#set($symbol_pound='#')#set($symbol_dollar='$')#set($symbol_escape='\')
+
+package ${package};
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
+
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
+/**
+ * Unit tests for ${artifactId}.
+ * @author 
+ *
+ */
+public class ${componentClassname}Test{
+// TODO
+}
