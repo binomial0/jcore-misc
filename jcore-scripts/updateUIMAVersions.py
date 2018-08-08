69fe7ce7e00f20d151637ed533e027cb5f27329d
Kaspar Kasche <mail@kasparkasche.de>
2018-08-08 15:47:38 +0200

    Modified updateUIMAVersions.py to create a version tag if none exist.

--- jcore-scripts/updateUIMAVersions.py
+++ jcore-scripts/updateUIMAVersions.py
@@ -21,6 +21,30 @@ def get_xmlns(tag_name):
         return ''
 
 
+def uima_file_type(tag_name):
+    uima_files = {
+        "{http://uima.apache.org/resourceSpecifier}collectionReaderDescription": 1,
+        "{http://uima.apache.org/resourceSpecifier}analysisEngineDescription": 2,
+        "{http://uima.apache.org/resourceSpecifier}casConsumerDescription": 3,
+        "{http://uima.apache.org/resourceSpecifier}typeSystemDescription": 4,
+        "{http://uima.apache.org/resourceSpecifier}typePriorities": 5,
+    }
+    return uima_files.get(tag_name, None)
+
+def get_uima_version_parent(root):
+    type = uima_file_type(root.tag)
+    if type == 4 or type == 5:
+        return root
+    else:
+        element_names = {
+            1: "{http://uima.apache.org/resourceSpecifier}processingResourceMetaData",
+            2: "{http://uima.apache.org/resourceSpecifier}analysisEngineMetaData",
+            3: "{http://uima.apache.org/resourceSpecifier}processingResourceMetaData",
+        }
+        name = element_names.get(type)
+        return root.find(name)
+
+
 def process_directory(dirname):
     for entry in os.scandir(dirname):
         if entry.is_dir():
@@ -35,21 +59,36 @@ def process_directory(dirname):
                 # print(xmlns)
                 if xmlns:
                     ET.register_namespace("", xmlns)
+                else:
+                    ET.register_namespace("", "")
 
                 modified = False
 
-                for elem in tree.iter("{http://uima.apache.org/resourceSpecifier}version"):
-                    # print(elem.text)
-                    old_version = elem.text
-                    elem.text = new_version
-                    modified = True
-                    print("changed version", old_version, "to", new_version, "in", entry.path)
+                version_tag_name = "{http://uima.apache.org/resourceSpecifier}version"
+
+                if uima_file_type(root.tag):
+                    parent = get_uima_version_parent(root)
+                    if parent:
+                        elem = parent.find(version_tag_name)
+                        if elem is not None:
+                            old_version = elem.text
+                            elem.text = new_version
+                            print("changed version", old_version, "to", new_version, "in", entry.path)
+                        else:
+                            elem = ET.SubElement(parent, version_tag_name)
+                            elem.text = new_version
+                            print("added version", new_version, "in", entry.path)
+                        modified = True
+                    else:
+                        print("Error: don't understand structure of", entry.path)
 
                 if modified:
                     tree.write(entry.path, xml_declaration=True, encoding="UTF-8")
 
             except Exception as e:
                 print(e)
+                print("orrured in: ", entry.path)
 
 
 process_directory(path)
+
